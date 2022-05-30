55957c2f8648ac84685a23d2c354929150e8d828
josep andreu <josepa45@gmail.com>
2022-05-30 18:48:19 +0200

    Connected to database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
+const debug = require("debug")("checkme:root");
 
+const chalk = require("chalk");
+const connectDB = require("./database");
 const initializeServer = require("./server/initializeServer");
 
 const port = process.env.PORT ?? 4000;
+const connectionString = process.env.MONGODB_STRING;
 
-initializeServer(port);
+(async () => {
+  try {
+    await connectDB(connectionString);
+    await initializeServer(port);
+  } catch {
+    debug(chalk.red("An error ocurred"));
+  }
+})();
