ecb62320b3eca7ac48aea6a38d57abf8c151af12
josep andreu <josepa45@gmail.com>
2022-06-06 16:47:58 +0200

    Tested endpoint delete

--- src/server/routers/checksRouters/checksRouters.js
+++ src/server/routers/checksRouters/checksRouters.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   getChecks,
+  deleteCheck,
 } = require("../../controllers/checksController/checksController");
 
 const router = express.Router();
 
 router.get("/", getChecks);
+router.delete("/:idCheck", deleteCheck);
 
 module.exports = router;
